4a29d7103837ecf33cbca7186c504aaae6dfff9b
kel-man <kyyafuso@email.com>
2021-08-31 10:42:29 -0700

    adds seeds for development database testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,79 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = [
+  {
+    email: 'shawn@users.com',
+    password: 'user1password!'
+  },
+  {
+    email: 'jason@users.com',
+    password: 'user2password!'
+  }
+]
+
+users.each do |attr|
+  User.create attr
+  p "creating user #{attr}"
+end
+
+apartments = [
+  {
+    street: '11211 E 132nd St.',
+    city: 'Gresham',
+    state: 'OR',
+    manager: 'Shawn',
+    email: 'shawn@apartmentsor.com',
+    price: '$1200.00 / month',
+    bedrooms: 2,
+    bathrooms: 2,
+    pets: 'Pets permitted with deposit'
+  },
+  {
+    street: '10398 E Washington St.',
+    city: 'Portland',
+    state: 'OR',
+    manager: 'Shawn',
+    email: 'shawn@apartmentsor.com',
+    price: '$1800.00 / month',
+    bedrooms: 2,
+    bathrooms: 1,
+    pets: 'Large animals not permitted.'
+  },
+  {
+    street: '1400 W 142nd St.',
+    city: 'Beaverton',
+    state: 'OR',
+    manager: 'Jason',
+    email: 'jason@apartmentsor.com',
+    price: '$1400.00 / month',
+    bedrooms: 2,
+    bathrooms: 2,
+    pets: 'Pets permitted with deposit'
+  },
+  {
+    street: '16390 W 86th St.',
+    city: 'Tigard',
+    state: 'OR',
+    manager: 'Jason',
+    email: 'jason@apartmentsor.com',
+    price: '$1600.00 / month',
+    bedrooms: 3,
+    bathrooms: 2,
+    pets: 'Pets permitted with deposit'
+  }
+]
+
+shawn = User.where(email: 'shawn@users.com').first
+jason = User.where(email: 'jason@users.com').first
+
+apartments.each_with_index do |attr, index|
+  if index < 2
+    shawn.apartments.create attr
+    p "creating apartment #{attr}"
+  else
+    jason.apartments.create attr
+    p "creating apartment #{attr}"
+  end
+end
